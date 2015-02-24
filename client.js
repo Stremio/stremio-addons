40746e69c2ef213a64d11534d40e6c34954c49f6
Ivo Georgiev <ivo@linvo.org>
2015-02-24 20:00:49 +0200

    Significant skeleton advancements

--- client.js
+++ client.js
@@ -3,34 +3,56 @@ var async = require("async");
 var jayson = require("jayson");
 
 function bindDefaults(call) {
-	meta: {
-		get: call.bind(null, "meta.get"),
-		find: call.bind(null, "meta.find"),
-		search: call.bind(null, "meta.search")
-	},
-	index: { 
-		get: call.bind(null, "index.get")
-	},
-	stream: {
-		get: call.bind(null, "stream.get")
+	return {
+		meta: {
+			get: call.bind(null, "meta.get"),
+			find: call.bind(null, "meta.find"),
+			search: call.bind(null, "meta.search")
+		},
+		index: { 
+			get: call.bind(null, "index.get")
+		},
+		stream: {
+			get: call.bind(null, "stream.get")
+		}		
 	}
 };
 
+function Service(url, options, client)
+{
+	this.client = client({ url: url });
+	this.url = url;
+	this.priority = options.priority || 0;
+
+	this.call = function(method, cb)
+	{
+
+	};
+};
 
 function Stremio(options)
 {
 	var services = {} ;
 
 	// Adding services
-	this.addService = function(url) {
+	this.addService = function(url, opts) {
 		if (services[url]) return;
-		services[url] = {
-			client: (options.client || jayson.client.http)({ url: url+module.parent.STREMIO_PATH })
-		};
+		services[url] = new Service(url, opts, options.client || jayson.client.http);
 	};
 
 	// Bind methods
-	function call(method, cb) { /*q.push({ method: method, cb: cb })*/ };
+	function call(method, cb) {
+		var keys = Object.keys(services).sort(function(a,b) { return services[b].priority - services[a].priority });
+		async.each(keys, function(key, next) {
+			var service = services[key];
+			service.call(method, function(skip, err, res) {
+				if (skip) return next(); // Go to the next service
+
+				cb(err, res);
+				next(1); // Stop
+			});
+		});
+	};
 	_.extend(this, bindDefaults(call))
 	this.call = call;
 };
