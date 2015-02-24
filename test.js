2286539ab99b84170853cbd33db9e5c05c1a47e4
Ivo Georgiev <ivo@linvo.org>
2015-02-24 20:45:50 +0200

    Example written

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
+var services = require("./");
 
-// TEMP
-var s new Stremio();
+// SERVER
+var server = new services.Server({
+	"meta.get": function(args, cb) {
+		console.log("received args -> "+args);
+		return cb(null, { now: Date.now() });
+	}
+});
+server.listen(3009);;
+
+// CLIENT
+var s new services.Client();
 s.addService("http://localhost:3009");
-s.call("meta.get",{ id: 1 }, function(err, res){
+s.call("meta.get", { id: 1 }, function(err, res)
+{
 	console.log(err,res);
-})
+});
+
