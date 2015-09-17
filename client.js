f2b63a3348a83c1d8c6334441f6059d0c9124fe1
Ivo Georgiev <ivo@linvo.org>
2015-09-17 19:40:20 +0300

    initialized / networkErr sort is now most important

--- client.js
+++ client.js
@@ -147,11 +147,11 @@ function Stremio(options)
 	
 	// Listing
 	this.get = function(forMethod, forArgs) {
-		var res = _.chain(services).values().sortBy(function(x){ return x.priority }).sortBy(function(x) { return -(x.initialized && !x.networkErr) }).value();
+		var res = _.chain(services).values().sortBy(function(x){ return x.priority }).value();
 		if (forMethod) res = res.filter(function(x) { return x.initialized ? x.methods.indexOf(forMethod) != -1 : true }); // if it's not initialized, assume it supports the method
 		if (forMethod) res = picker(res, forMethod); // apply the picker for a method
 		if (forArgs) res = _.sortBy(res, function(x) { return -checkArgs(forArgs, x.manifest.filter) });
-		return res;
+		return _.sortBy(res, function(x) { return -(x.initialized && !x.networkErr) });
 	};
 
 	// Set de-bounced batching
