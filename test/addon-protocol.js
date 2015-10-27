48823e7a778363e0f4dc6b4b7a03060309c855cc
Ivo Georgiev <ivo@linvo.org>
2015-10-27 18:31:20 +0200

    add addon-protocol tests template

---
+++ test/addon-protocol.js
@@ -0,0 +1,8 @@
+// Test if an add-on implements the Stremio protocol OK and responds
+//tape("meta.find - get top 100 items")
+//tape("meta.find - collect genres")
+//tape("meta.find - particular genre")
+
+//tape("stats.get responds")
+
+//tape("stream.find responds")
