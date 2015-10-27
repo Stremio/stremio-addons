62a74b224b80e4a3efcde22e94f89c0ed5e85d9f
Ivo Georgiev <ivo@linvo.org>
2015-10-27 18:56:36 +0200

    addon-protocol: parse args

--- test/addon-protocol.js
+++ test/addon-protocol.js
@@ -1,3 +1,20 @@
+var addons = require("../");
+var tape = require("tape");
+var http = require("http");
+var _ = require("lodash");
+
+
+var addons = process.argv.filter(function(x) { return x.match("^http") });
+if (! addons.length) throw "No add-ons specified";
+
+var slackPush;
+process.argv.forEach(function(x) { 
+	if (x.match("--slack-push")) slackPush = x.split("=")[1];
+});
+
+addons.forEach(function(addon) {
+
+});
 // Test if an add-on implements the Stremio protocol OK and responds
 //tape("meta.find - get top 100 items")
 //tape("meta.find - collect genres")
