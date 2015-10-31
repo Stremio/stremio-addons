5487d11f9498acf15bc322e208e175dac66834a3
Ivo Georgiev <ivo@strem.io>
2015-10-31 22:21:23 +0200

    Changing central

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.STREMIO_PATH = "/stremio/v1";
-module.CENTRAL = "http://api8.herokuapp.com";
+module.CENTRAL = "http://api9.strem.io";
 
 module.exports.Client = require("./client");
 module.exports.Server = require("./server");
