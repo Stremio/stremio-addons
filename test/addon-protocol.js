a003095350ae18cc7e04c6de612a56ff7c585932
Ivo Georgiev <ivo@linvo.org>
2016-01-10 13:52:05 +0200

    Fixes to testing script

--- test/addon-protocol.js
+++ test/addon-protocol.js
@@ -3,7 +3,7 @@ var tape = require("tape");
 var _ = require("lodash");
 var async = require("async");
 
-var NETWORK_TIMEOUT = 30*1000;
+var NETWORK_TIMEOUT = 35*1000;
 
 var TEST_SECRET = "51af8b26c364cb44d6e8b7b517ce06e39caf036a";
 
@@ -19,37 +19,59 @@ process.argv.forEach(function(x) {
 	if (x.match("--short-log")) shortLog = true;
 });
 
-var hasErr = false, output = [];
-
-var topitems = []; // global, so we can test meta and stream add-ons at once
+var topitems = [], hasErr; // global, so we can test meta and stream add-ons at once
 
 async.eachSeries(addons, function(url, ready) {
 	var test = tape.createHarness();
 
-	var s = new stremio.Client({ timeout: NETWORK_TIMEOUT });
-	s.add(url, { priority: 1 });
-	s.setAuth(null, TEST_SECRET);
+	/* Send errors to Slack webhook
+	 */
+	var errors = 0, output = [];
+	// WARNING: we can't do createStream more than once, because it bugs the first test
+	test.createStream({ /* objectMode: true */ }).on("data", function(x) {
+		if (x.match("^not ok")) { errors++; hasErr = true }
+		output.push(x);
+		process.stdout.write(x);
+	}).on("end", function() {
+		if (errors < 2) return ready();
+		if (!slackPush) return ready();
+				
+		var body = require("querystring").stringify({ payload: JSON.stringify({ 
+			channel: slackChannel || "#mon-stremio", username: "webhookbot",
+			text: "*WARNING: "+url+" failing "+(slackMessage || "")+" with "+errors+" errors *\n",
+			icon_emoji: ":bug:"
+		}) });
 
-	test("simple http request", function(t) {
-		var req = require("http").request(require("url").parse(url), function(resp) {
-			t.ok(resp, "has response");
-			t.end();
-		}).on("error", function(err) {
-			t.error(err);
-			t.end();
-		}).on("timeout", function() { t.error("network timeout"); t.end() });
-		req.setTimeout(15000);	
+		console.log("Sending errors to slack");
+		var req = require("https").request(_.extend(require("url").parse(slackPush), { 
+			headers: { "Content-Type": "application/x-www-form-urlencoded", "Content-Length": body.length },
+			method: "POST"
+		}), function() {
+			console.log("Sent errors to slack");
+			ready();
+		});
+		req.write(body);
+		req.end();
 	});
+
+
+	var s = new stremio.Client({ timeout: NETWORK_TIMEOUT });
+	s.setAuth(null, TEST_SECRET);
 
 	var LID;
 	test("is available - fires addon-ready", function(t) {
-		t.comment("Testing "+url);
-		s.on("addon-ready", function(addon) {
+		s.add(url, { priority: 1 });
+
+		var timeout = setTimeout(function() { t.error("timed out"); t.end() }, 10000)
+
+		s.once("addon-ready", function(addon) {
+			clearTimeout(timeout);
 			t.ok(addon && addon.url == url, "has proper url");
 			if (addon.manifest.stremio_LID) LID = addon.manifest.stremio_LID; 
 			t.end();
 		});
 	});
+
 
 	test("stats.get responds", function(t) {
 		if (noStats) { t.skip("--no-stats"); t.end(); return }
@@ -140,40 +162,6 @@ async.eachSeries(addons, function(url, ready) {
 	//test("meta.find - particular genre")
 	//test("meta.find - returns valid results") // copy from filmon addon
 
-	/* Send errors to Slack webhook
-	 */
-	test.createStream({ /* objectMode: true */ }).on("data", function(x) {
-		if (x.match("^not ok")) hasErr = true;
-		output.push(x);
-		//if (x.hasOwnProperty("ok") && !x.ok) errors.push(x);
-	}).on("end", function() {
-		if (! hasErr) return ready();
-		if (!slackPush) return ready();
-		
-		if (shortLog) output = output.slice(-5);
-		
-		var body = require("querystring").stringify({ payload: JSON.stringify({ 
-			channel: slackChannel || "#mon-stremio", username: "webhookbot",
-			text: "*WARNING: "+url+" failing "+(slackMessage || "")+" *\n```"+output.join("\n")+"```\n",
-			icon_emoji: ":bug:"
-		}) });
-
-		console.log("Sending errors to slack");
-		var req = require("https").request(_.extend(require("url").parse(slackPush), { 
-			headers: { "Content-Type": "application/x-www-form-urlencoded", "Content-Length": body.length },
-			method: "POST"
-		}), function() {
-			console.log("Sent errors to slack");
-			ready();
-		});
-		req.write(body);
-		req.end();
-	});
-
-	/* Default stream
-	 */
-	console.log("\n\n");
-	test.createStream().pipe(process.stdout); // pipe to stdout
 }, function() {
 	process.exit(hasErr ? 1 : 0);
 });
