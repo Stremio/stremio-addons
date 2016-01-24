2ad895666f5fa49acbea9bfb1efa458e8cdf96db
Ivo Georgiev <ivo@linvo.org>
2016-01-25 00:51:32 +0200

    stremio-addons.js update

--- browser/stremio-addons.js
+++ browser/stremio-addons.js
@@ -1,8 +1,6 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (Buffer){
 var _ = require("lodash");
 var async = require("async");
-var utils = require("./utils");
 var dot = require("dot-object");
 var url = require("url");
 var emitter = require("tiny-emitter");
@@ -51,7 +49,7 @@ function Addon(url, options, stremio, ready)
 {
 	var self = this;
 
-	var client = options.client || rpcClient;
+	var client = options.client || require("./rpc");
 	this.client = client(url+(module.parent ? module.parent.STREMIO_PATH : "/stremio/v1") , { 
 		timeout: options.timeout || stremio.options.timeout || 10000,
 		respTimeout: options.respTimeout || stremio.options.respTimeout //|| 10000,
@@ -238,70 +236,10 @@ function getTypes(services) {
 	return types;
 };
 
-// Utility for JSON-RPC
-// Rationales in our own client
-// 1) have more control over the process, be able to implement debounced batching
-// 2) reduce number of dependencies
-function rpcClient(endpoint, options)
-{
-	var isGet = !!endpoint.match("stremioget");
-
-	var client = { };
-	client.request = function(method, params, callback) {
-		rpcRequest([{ callback: callback, params: params, method: method, id: utils.genID(), jsonrpc: "2.0" }]);
-	};
-	if (!isGet) client.enqueue = function(handle, method, params, callback) {
-		if (! handle.flush) handle.flush = _.debounce(function() {
-			rpcRequest(handle.queue); handle.queue = [];
-		}, handle.time);
-		handle.queue.push({ callback: callback, params: params, method: method, id: utils.genID(), jsonrpc: "2.0" });
-		handle.flush();
-	};
-	function rpcRequest(requests) { // supports batching
-		requests.forEach(function(x, i) { 
-			x.callback = _.once(x.callback);
-			if (isGet) x.params[0] = null; // get requests limited to noauth
-			if (isGet) x.id = i+1; // unify ids
-		});
-
-		var body = JSON.stringify(requests.length == 1 ? requests[0] : requests);
-		var byId = _.indexBy(requests, "id");
-		var callbackAll = function() { var args = arguments; requests.forEach(function(x) { x.callback && x.callback.apply(null, args) }) };
-
-		if (body.length>=LENGTH_TO_FORCE_POST) isGet = false;
-
-		var reqObj = { };
-		if (!isGet) _.extend(reqObj, url.parse(endpoint), { method: "POST", headers: { "Content-Type": "application/json", "Content-Length": body.length } });
-		else _.extend(reqObj, url.parse(endpoint+"/q.json?b="+new Buffer(body, "binary").toString("base64")));
-		
-		var req = utils.http.request(reqObj, function(res) {
-			if (options.respTimeout && res.setTimeout) res.setTimeout(options.respTimeout);
-
-			utils.receiveJSON(res, function(err, body) {
-				if (err) return callbackAll(err);
-				//console.log(res.headers["cf-cache-status"]);
-				(Array.isArray(body) ? body : [body]).forEach(function(body, i) {
-					var callback = (byId[body.id] && byId[body.id].callback) || (requests[i] && requests[i].callback) || _.noop; // WARNING with noop
-					if (body.error) return callback(null, body.error);
-					if (!body.result) return callback(body);
-					callback(null, null, body.result);
-				});
-			});
-		});
-
-		if (options.timeout && req.setTimeout) req.setTimeout(options.timeout);
-		req.on("error", callbackAll);
-		req.on("timeout", function() { callbackAll(new Error("rpc request timed out")) });
-		if (! isGet) req.write(body);
-		req.end();
-	};
-	return client;
-};
 
 module.exports = Stremio;
 
-}).call(this,require("buffer").Buffer)
-},{"./utils":46,"async":2,"buffer":4,"dot-object":42,"inherits/inherits_browser":43,"lodash":44,"tiny-emitter":45,"url":40}],2:[function(require,module,exports){
+},{"./rpc":46,"async":2,"dot-object":42,"inherits/inherits_browser":43,"lodash":44,"tiny-emitter":45,"url":40}],2:[function(require,module,exports){
 (function (process){
 /*!
  * async
@@ -20188,10 +20126,13 @@ module.exports = E;
 
 },{}],46:[function(require,module,exports){
 (function (Buffer){
-module.exports.http = require("http");
+var http = require("http");
 var url = require("url");
+var _ = require("lodash");
 
-module.exports.receiveJSON = function(resp, callback) {
+var LENGTH_TO_FORCE_POST=8192;
+
+var receiveJSON = function(resp, callback) {
 	if (resp.method == "GET") {
 		var body = url.parse(resp.url, true).query.b;
 		try { body = JSON.parse(new Buffer(body, "base64").toString()) } catch(e) { return callback(e) };
@@ -20206,12 +20147,77 @@ module.exports.receiveJSON = function(resp, callback) {
 	});
 };
 
-module.exports.genID = function() { 
+var genID = function() { 
 	return Math.round(Math.random() * Math.pow(2, 24)) 
 };
 
+// Utility for JSON-RPC
+// Rationales in our own client
+// 1) have more control over the process, be able to implement debounced batching
+// 2) reduce number of dependencies
+function rpcClient(endpoint, options)
+{
+	var isGet = !!endpoint.match("stremioget");
+
+	var client = { };
+	client.request = function(method, params, callback) {
+		rpcRequest([{ callback: callback, params: params, method: method, id: genID(), jsonrpc: "2.0" }]);
+	};
+	if (!isGet) client.enqueue = function(handle, method, params, callback) {
+		if (! handle.flush) handle.flush = _.debounce(function() {
+			rpcRequest(handle.queue); handle.queue = [];
+		}, handle.time);
+		handle.queue.push({ callback: callback, params: params, method: method, id: genID(), jsonrpc: "2.0" });
+		handle.flush();
+	};
+	function rpcRequest(requests) { // supports batching
+		requests.forEach(function(x, i) { 
+			x.callback = _.once(x.callback);
+			if (isGet) x.params[0] = null; // get requests limited to noauth
+			if (isGet) x.id = i+1; // unify ids
+		});
+
+		var body = JSON.stringify(requests.length == 1 ? requests[0] : requests);
+		var byId = _.indexBy(requests, "id");
+		var callbackAll = function() { var args = arguments; requests.forEach(function(x) { x.callback && x.callback.apply(null, args) }) };
+
+		if (body.length>=LENGTH_TO_FORCE_POST) isGet = false;
+
+		var reqObj = { };
+		if (!isGet) _.extend(reqObj, url.parse(endpoint), { method: "POST", headers: { "Content-Type": "application/json", "Content-Length": body.length } });
+		else _.extend(reqObj, url.parse(endpoint+"/q.json?b="+new Buffer(body, "binary").toString("base64")));
+		
+		var req = http.request(reqObj, function(res) {
+			if (options.respTimeout && res.setTimeout) res.setTimeout(options.respTimeout);
+
+			receiveJSON(res, function(err, body) {
+				if (err) return callbackAll(err);
+				//console.log(res.headers["cf-cache-status"]);
+				(Array.isArray(body) ? body : [body]).forEach(function(body, i) {
+					var callback = (byId[body.id] && byId[body.id].callback) || (requests[i] && requests[i].callback) || _.noop; // WARNING with noop
+					if (body.error) return callback(null, body.error);
+					if (!body.result) return callback(body);
+					callback(null, null, body.result);
+				});
+			});
+		});
+
+		if (options.timeout && req.setTimeout) req.setTimeout(options.timeout);
+		req.on("error", callbackAll);
+		req.on("timeout", function() { callbackAll(new Error("rpc request timed out")) });
+		if (! isGet) req.write(body);
+		req.end();
+	};
+	return client;
+};
+
+module.exports = rpcClient;
+module.exports.genID = genID;
+module.exports.receiveJSON = receiveJSON;
+module.exports.http = http;
+
 }).call(this,require("buffer").Buffer)
-},{"buffer":4,"http":30,"url":40}],"stremio-addons":[function(require,module,exports){
+},{"buffer":4,"http":30,"lodash":44,"url":40}],"stremio-addons":[function(require,module,exports){
 module.STREMIO_PATH = "/stremio/v1";
 module.CENTRAL = "http://api9.strem.io";
 module.exports.Client = require("./client");
