e47fa915a24d8df3f9502b6e71384a36e761ed8d
Ivo Georgiev <ivo@linvo.org>
2016-06-02 20:13:44 +0300

    use underscore instead of lodash

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-var _ = require("lodash");
+var _ = require("underscore");
 var async = require("async");
 var dot = require("dot-object");
 var url = require("url");
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var _ = require("lodash");
+var _ = require("underscore");
 var url = require("url");
 var rpc = require("./rpc");
 var async = require("async");
