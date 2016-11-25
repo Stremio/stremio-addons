cc1432691fc528e033d96f326945908932903209
Ivo Georgiev <ivo@linvo.org>
2016-11-25 22:04:57 +0200

    use console.log instead of console.warning

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ function Server(methods, options, manifest)
 
 		// TODO: cache at least stats.get for some time
 		self.request("stats.get", [{ stremioget: true }], function(err, s) {
- 			if (err) console.warning(err);
+ 			if (err) console.log(err);
 			if (s) stats = s;
 			self.request("meta.find", [{stremioget: true}, { query: {}, limit: 10 }], function(err, t) {
 				if (err) return error(err);
