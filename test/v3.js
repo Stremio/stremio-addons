7a91329064b25d91752fc3bd69f9a3e1f12bc669
Ivo Georgiev <ivo@linvo.org>
2018-03-16 10:26:33 +0200

    SIP: test promisifed

--- test/v3.js
+++ test/v3.js
@@ -4,6 +4,7 @@ const AddonClient = require('../lib/client')
 const errors = require('../lib/errors')
 
 const tape = require('tape')
+const promisify = require('util').promisify
 
 tape('detectFromURL: invalid protocol', function(t) {
 	AddonClient.detectFromURL('ftp://cinemeta.strem.io', function(err, res) {
@@ -67,31 +68,49 @@ tape('detectFromURL: detect and use manifest.json URL', function(t) {
 })
 
 
+// @TODO: promisify
 tape('detectFromURL: detect and use manifest.json URL', function(t) {
 	// ipfs://QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json
 	// ipns://QmYRaTC2DqsgXaRUJzGFagLy725v1QyYwt66kvpifPosgj/manifest.json
-	AddonClient.detectFromURL('ipfs://QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json', function(err, res) {
-		t.error(err, 'no error from detectFromURL')
+
+	const detectFromURL = promisify(AddonClient.detectFromURL)
+	let addon 
+	let addonGet
+	detectFromURL('ipfs://QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json')
+	.then(function(res) {
 		t.ok(res.addon, 'addon is ok')
 		t.ok(res.addon.manifest, 'manifest is ok')
 		t.deepEqual(res.addon.manifest.catalogs, ['top'], 'catalogs is right')
 		t.deepEqual(res.addon.manifest.resources, ['meta', 'stream'], 'resources is right')
 
-		res.addon.get('catalog', 'top', function(err, resp) {
-			t.error(err, 'no error from catalog')
-			t.ok(Array.isArray(resp), 'response is an array')
+		addon = res.addon
+		addonGet = promisify(res.addon.get)
 
-			res.addon.get('meta', resp[0].type, resp[0].id, function(err, meta) {
-				t.error(err, 'no error from meta')
-				t.ok(meta.id, 'meta has id')
+		return addonGet('catalog', 'top')
+	})
+	.then(function(resp) {
+		t.ok(Array.isArray(resp), 'response is an array')
+		return addonGet('meta', resp[0].type, resp[0].id)
+	})
+	.then(function(meta) {
+		t.ok(meta.id, 'meta has id')
 
-				// IPFS addons need to be destroyed in order to allow the proc to exit
-				res.addon.destroy(function()
-				{
-					t.end()
-				})
-			})
-		})
+		//return addonGet('stream', 'movie', parseInt(Math.random()*1000))
+
+		// IPFS addons need to be destroyed in order to allow the proc to exit
+		addon.destroy(function() { t.end() })
+	})
+	/*
+	.then(function(stream) {
+		console.log(stream)
+		// IPFS addons need to be destroyed in order to allow the proc to exit
+		addon.destroy(function() { t.end() })
+	})*/
+	.catch(function(err) {
+		t.error(err, 'no error')
+
+		// IPFS addons need to be destroyed in order to allow the proc to exit
+		addon.destroy(function() { t.end() })
 	})
 })
 
