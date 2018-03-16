4bba40049fd2bbeb7e70a1700ae012da6c6bb26f
Ivo Georgiev <ivo@linvo.org>
2018-03-16 11:07:36 +0200

    SIP: Promisification

--- sip/TODO.md
+++ sip/TODO.md
@@ -3,13 +3,13 @@
 * draft new client lib with `legacy` support; figure out the architecture of detection and etc.
 * Write IPFS-based proto
 * see if IPFS pubsub/room has signatures - it has peerId, so it should be secure - there are NO SIGNATURES
+* implement built-in promisification
 
 ## TODO
 
 * solve the js-ipfs lock issue so we can test N instances at the same time - see ipfs tests and etc
 * consider localtunnel (lt) instead of pubsub, will be easier and more anonymous
 * or a very simple pubsub impl
-* implement built-in promisification
 * learn how WebRTC works and whether we can provide nodes directly in the manifest (by peer ID and possibly addr)
 * IPFS impl to use pubusb to get missing; also re-eval the pubsub model, perhaps sending a message to someone in the swarm is sufficient
 * IPFS-based SDK implementation
--- test/v3.js
+++ test/v3.js
@@ -4,7 +4,6 @@ const AddonClient = require('../lib/client')
 const errors = require('../lib/errors')
 
 const tape = require('tape')
-const promisify = require('util').promisify
 
 tape('detectFromURL: invalid protocol', function(t) {
 	AddonClient.detectFromURL('ftp://cinemeta.strem.io', function(err, res) {
@@ -41,42 +40,9 @@ tape('detectFromURL: legacy protocol', function(t) {
 tape('detectFromURL: detect and use manifest.json URL', function(t) {
 	// https://gateway.ipfs.io/ipfs/QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json
 	// https://gateway.ipfs.io/ipns/QmYRaTC2DqsgXaRUJzGFagLy725v1QyYwt66kvpifPosgj/manifest.json
-	AddonClient.detectFromURL('https://gateway.ipfs.io/ipfs/QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json', function(err, res) {
-		t.error(err, 'no error from detectFromURL')
-		t.ok(res.addon, 'addon is ok')
-		t.ok(res.addon.manifest, 'manifest is ok')
-		t.deepEqual(res.addon.manifest.catalogs, ['top'], 'catalogs is right')
-		t.deepEqual(res.addon.manifest.resources, ['meta', 'stream'], 'resources is right')
 
-		res.addon.get('catalog', 'top', function(err, resp) {
-			t.error(err, 'no error from catalog')
-			t.ok(Array.isArray(resp), 'response is an array')
-
-			res.addon.get('meta', resp[0].type, resp[0].id, function(err, meta) {
-				t.error(err, 'no error from meta')
-				t.ok(meta.id, 'meta has id')
-
-				res.addon.get('stream', meta.type, meta.id, function(err, streams) {
-					t.error(err, 'no error from streams')
-					t.ok(Array.isArray(streams), 'streams is array')
-					t.equal(streams.length, 2, 'streams is right length')
-					t.end()
-				})
-			})
-		})
-	})
-})
-
-
-// @TODO: promisify
-tape('detectFromURL: detect and use manifest.json URL', function(t) {
-	// ipfs://QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json
-	// ipns://QmYRaTC2DqsgXaRUJzGFagLy725v1QyYwt66kvpifPosgj/manifest.json
-
-	const detectFromURL = promisify(AddonClient.detectFromURL)
-	let addon 
-	let addonGet
-	detectFromURL('ipfs://QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json')
+	let addon
+	AddonClient.detectFromURL('https://gateway.ipfs.io/ipfs/QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json')
 	.then(function(res) {
 		t.ok(res.addon, 'addon is ok')
 		t.ok(res.addon.manifest, 'manifest is ok')
@@ -84,20 +50,57 @@ tape('detectFromURL: detect and use manifest.json URL', function(t) {
 		t.deepEqual(res.addon.manifest.resources, ['meta', 'stream'], 'resources is right')
 
 		addon = res.addon
-		addonGet = promisify(res.addon.get)
 
-		return addonGet('catalog', 'top')
+		return addon.get('catalog', 'top')
 	})
 	.then(function(resp) {
 		t.ok(Array.isArray(resp), 'response is an array')
-		return addonGet('meta', resp[0].type, resp[0].id)
+
+		return addon.get('meta', resp[0].type, resp[0].id)
 	})
 	.then(function(meta) {
 		t.ok(meta.id, 'meta has id')
 
-		//return addonGet('stream', 'movie', parseInt(Math.random()*1000))
+		return addon.get('stream', meta.type, meta.id)
+	})
+	.then(function(streams) {
+		t.ok(Array.isArray(streams), 'streams is array')
+		t.equal(streams.length, 2, 'streams is right length')
+		t.end()
+	})
+	.catch(function(err) {
+		t.error(err, 'no error')
+		t.end()
+	})
+})
 
-		// IPFS addons need to be destroyed in order to allow the proc to exit
+
+tape('detectFromURL: detect and use manifest.json URL', function(t) {
+	// ipfs://QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json
+	// ipns://QmYRaTC2DqsgXaRUJzGFagLy725v1QyYwt66kvpifPosgj/manifest.json
+
+	let addon 
+	AddonClient.detectFromURL('ipfs://QmTQTixUrtf9E4fasjes5Jb1o956FF6xqSsXnwrc5GLKeB/manifest.json')
+	.then(function(res) {
+		t.ok(res.addon, 'addon is ok')
+		t.ok(res.addon.manifest, 'manifest is ok')
+		t.deepEqual(res.addon.manifest.catalogs, ['top'], 'catalogs is right')
+		t.deepEqual(res.addon.manifest.resources, ['meta', 'stream'], 'resources is right')
+
+		addon = res.addon
+
+		return addon.get('catalog', 'top')
+	})
+	.then(function(resp) {
+		t.ok(Array.isArray(resp), 'response is an array')
+		return addon.get('meta', resp[0].type, resp[0].id)
+	})
+	.then(function(meta) {
+		t.ok(meta.id, 'meta has id')
+
+		// @TODO: get a non-cached item
+		//return addon.get('stream', 'movie', parseInt(Math.random()*1000))
+
 		addon.destroy(function() { t.end() })
 	})
 	/*
